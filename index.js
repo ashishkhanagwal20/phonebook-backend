8c921acae75211c4d5aeb8f32537702dd659f522
ashishkhanagwal20 <ashishstudy20@gmail.com>
2023-12-07 19:56:36 +0530

    mongodDBconnect

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const Phonebook = require("./models/phonebook");
+
+const morgan = require("morgan");
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("dist"));
@@ -27,20 +34,29 @@ let phonebook = [
     number: "39-23-6423122",
   },
 ];
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
 app.get("/api/persons", (req, res) => {
-  res.json(phonebook);
+  Phonebook.find({}).then((phonebook) => {
+    res.json(phonebook);
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const persons = phonebook.find((person) => {
-    return person.id === id;
+// app.get("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const persons = phonebook.find((person) => {
+//     return person.id === id;
+//   });
+//   if (persons) {
+//     res.json(persons);
+//   } else {
+//     res.status(404).end("404 Page not found");
+//   }
+// });
+
+app.get("/api/persons/:id", (request, response) => {
+  Phonebook.findById(request.params.id).then((phonebook) => {
+    response.json(phonebook);
   });
-  if (persons) {
-    res.json(persons);
-  } else {
-    res.status(404).end("404 Page not found");
-  }
 });
 
 const generateId = () => {
@@ -49,37 +65,57 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+// app.post("/api/persons", (req, res) => {
+//   const body = req.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  };
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId(),
+//   };
 
+//   if (!body.name) {
+//     return res.status(400).json({
+//       error: "Name is Missing",
+//     });
+//   }
+//   if (!body.number) {
+//     return res.status(400).json({
+//       error: " Number is Missing",
+//     });
+//   }
+//   const name = phonebook.find((person) => {
+//     return person.name === body.name;
+//   });
+//   if (name) {
+//     return res.status(409).json({
+//       error: "Name aleady exists",
+//     });
+//   }
+
+//   phonebook = phonebook.concat(person);
+
+//   res.json(person);
+// });
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
   if (!body.name) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: "Name is Missing",
     });
   }
   if (!body.number) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: " Number is Missing",
     });
   }
-  const name = phonebook.find((person) => {
-    return person.name === body.name;
+  const phonebook = new Phonebook({
+    name: body.name,
+    number: body.number,
   });
-  if (name) {
-    return res.status(409).json({
-      error: "Name aleady exists",
-    });
-  }
-
-  phonebook = phonebook.concat(person);
-
-  res.json(person);
+  phonebook.save().then((person) => {
+    response.json(person);
+  });
 });
 
 app.get("/info", (req, res) => {
---
+++ models/phonebook.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+console.log("Connecting to : ", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB : ", error.message);
+  });
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+phonebookSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Phonebook", phonebookSchema);
